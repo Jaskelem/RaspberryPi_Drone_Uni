d20115ed5dfe8c633e2e1e0ec43408bfdd05a7b7
jaskelem <meldas.jaskelevicius@gmail.com>
2023-02-19 11:03:55 +0200

    gyro and acc modification

--- Acc_Gyro.py
+++ Acc_Gyro.py
@@ -4,21 +4,25 @@
 '''
 import smbus         #import SMBus module of I2C
 from time import sleep          #import
+import math
+
+bus = smbus.SMBus(1)    # or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
 
 def MPU_Init():
-
-    #some MPU6050 Registers and their Address
-    PWR_MGMT_1   = 0x6B
-    SMPLRT_DIV   = 0x19
-    CONFIG       = 0x1A
-    GYRO_CONFIG  = 0x1B
-    INT_ENABLE   = 0x38
-    ACCEL_XOUT_H = 0x3B
-    ACCEL_YOUT_H = 0x3D
-    ACCEL_ZOUT_H = 0x3F
-    GYRO_XOUT_H  = 0x43
-    GYRO_YOUT_H  = 0x45
-    GYRO_ZOUT_H  = 0x47
     
     #write to sample rate register
     bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
@@ -35,8 +39,6 @@ def MPU_Init():
     #Write to interrupt enable register
     bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
-    bus = smbus.SMBus(1)    # or bus = smbus.SMBus(0) for older version boards
-    Device_Address = 0x68   # MPU6050 device address
 
 def read_raw_data(addr):
     #Accelero and Gyro value are 16-bit
@@ -57,13 +59,13 @@ def GyroscopeInfo():
     gyro_y = read_raw_data(GYRO_YOUT_H)
     gyro_z = read_raw_data(GYRO_ZOUT_H)
 
-    Gx = round(gyro_x/131.0,1)
-    Gy = round(gyro_y/131.0,1)
-    Gz = round(gyro_z/131.0,1)
+    Gx = round(gyro_x/131.0,2)
+    Gy = round(gyro_y/131.0,2)
+    Gz = round(gyro_z/131.0,2)
 
-    Gx=Gx+0.1
-    Gy=Gy-0.3
-    Gz=Gz+0.005
+    Gx=Gx+0.12
+    Gy=Gy-0.23
+    #Gz=Gz+0.005
 
     return Gx,Gy,Gz
 
@@ -73,12 +75,13 @@ def AccelerometerInfo():
     acc_y = read_raw_data(ACCEL_YOUT_H)
     acc_z = read_raw_data(ACCEL_ZOUT_H)
 
-    Ax = round(acc_x/16384.0,2)
-    Ay = round(acc_y/16384.0,2)
-    Az = round(acc_z/16384.0,2)
+    Ax = round(acc_x/16384.0,3)
+    Ay = round(acc_y/16384.0,3)
+    Az = round(acc_z/16384.0,3)
 
-    Ay=Ay+0.052
-    Az=Az-1
+    Ax=Ax+0.06
+    Ay=Ay+0.03
+    Az=Az-0.9999
 
     return Ax,Ay,Az
 
@@ -100,15 +103,22 @@ def AverageInfo():
         TAy=+Ay
         TAz=+Az
         sleep(0.1)
-    
+    TGx=round(math.floor(TGx/5*1000)/1000,1)
+    TGy=round(math.floor(TGy/5*1000)/1000,1)
+    TGz=round(math.floor(TGz/5*1000)/1000,1)
+    TAx=round(math.floor(TAx/5*1000)/1000,2)
+    TAy=round(math.floor(TAy/5*1000)/1000,2)
+    TAz=round(math.floor(TAz/5*1000)/1000,2)
     return TGx,TGy,TGz,TAx,TAy,TAz
       
-
+def DifferentAllResult():
+    Gx,Gy,Gz,Ax,Ay,Az=AverageInfo()
+    if (Gx != 0 or Gy != 0 or Gz != 0 or Ax != 0 or Ay != 0 or Az != 0):
+        print ("Gx=%s" %Gx, u'\u00b0'+ "/s", "\tGy=%s" %Gy, u'\u00b0'+ "/s", "\tGz=%s" %Gz, u'\u00b0'+ "/s", "\tAx=%s g" %Ax, "\tAy=%s g" %Ay, "\tAz=%s g" %Az)
+    
 if __name__ == "__main__":
     
     MPU_Init()
-    print (" Reading Data of Gyroscope and Accelerometer")  
+    print (" Reading Data of Gyroscope and Accelerometer")
     while True:
-        Gx,Gy,Gz,Ax,Ay,Az=AverageInfo()
-        print ("Gx=%.1f" %Gx, u'\u00b0'+ "/s", "\tGy=%.1f" %Gy, u'\u00b0'+ "/s", "\tGz=%.1f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-        sleep(0.1)
+        DifferentAllResult()
