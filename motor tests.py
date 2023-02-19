fb7520bef724dbb17fe01984bb13e3abc33c0a90
jaskelem <meldas.jaskelevicius@gmail.com>
2023-02-19 04:19:18 +0200

    test

--- motor tests.py
+++ motor tests.py
@@ -13,26 +13,38 @@ p2 = GPIO.PWM(32,1000)
 p3 = GPIO.PWM(33,1000)
 p4 = GPIO.PWM(35,1000)
 
-p1.start(89)
-p2.start(89)
-p3.start(89)
-p4.start(89)
-
+p1.start(0)
+p2.start(0)
+p3.start(0)
+p4.start(0)
+'''
 p1.ChangeFrequency(188)
 p2.ChangeFrequency(188)
 p3.ChangeFrequency(188)
 p4.ChangeFrequency(188)
-
+'''
 time.sleep(3)
 
-for i in range(100,88,-1):
+for i in range(0,1000,1):
     p1.ChangeFrequency(i)
-    p2.ChangeFrequency(i)
-    p3.ChangeFrequency(i)
-    p4.ChangeFrequency(i)
     print("frequincy : %i" %i)
     time.sleep(1)
 p1.ChangeFrequency(188)
+
+for i in range(0,1000,1):
+    p2.ChangeFrequency(i)
+    print("frequincy : %i" %i)
+    time.sleep(1)
 p2.ChangeFrequency(188)
+
+for i in range(0,1000,1):
+    p3.ChangeFrequency(i)
+    print("frequincy : %i" %i)
+    time.sleep(1)
 p3.ChangeFrequency(188)
+
+for i in range(0,1000,1):
+    p4.ChangeFrequency(i)
+    print("frequincy : %i" %i)
+    time.sleep(1)
 p4.ChangeFrequency(188)
