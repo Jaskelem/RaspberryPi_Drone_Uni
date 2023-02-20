9bbdd89e05beae4f3fed825398e85dbdfa2b4834
jaskelem <meldas.jaskelevicius@gmail.com>
2023-02-20 09:41:40 +0200

    edit

--- configuremotor.py
+++ configuremotor.py
@@ -5,11 +5,11 @@
 
 import os     #importing os library so as to communicate with the system
 import time   #importing time library to make Rpi wait because its too impatient 
-os.system ("sudo pigpio-master/pigpiod") #Launching GPIO library
+os.system ("sudo pigpio") #Launching GPIO library
 time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
 import pigpio #importing GPIO library
 
-ESC=12  #Connect the ESC in this GPIO pin 
+ESC=4  #Connect the ESC in this GPIO pin 
 
 pi = pigpio.pi();
 pi.set_servo_pulsewidth(ESC, 0) 
